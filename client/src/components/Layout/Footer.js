5275db52850362ddf862c6bdb507cab3faa5f483
Bharani2004 <bharanikumarbk2004@gmail.com>
2024-07-09 10:36:04 +0530

    Client

---
+++ client/src/components/Layout/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <div className='footer'>
+      <h6 className='text-center'>All rights reserved &copy; Cash Caddy</h6>
+    </div>
+  );
+};
+
+export default Footer;
