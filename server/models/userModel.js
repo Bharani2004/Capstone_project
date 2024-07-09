f2b89243b40fcfb1348d0fd3b32ef7375a559d20
Ragavi2004 <ragavi24vlr@gmail.com>
2024-07-09 15:30:24 +0530

    models

---
+++ server/models/userModel.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose')
+
+//schema 
+const userSchema = new mongoose.Schema({
+    name: {
+        type:String,
+        required:[true,'Name is required']
+    },
+    email:
+    {
+        type:String,
+        required:[true,"Email is required and should be unique"],
+        unique:true,
+    },
+    password:
+    {
+        type:String,
+        required: [true, "Password is required"]
+    },
+ 
+    },
+    {timestamps:true}
+);
+
+//export
+const userModel = mongoose.model('user',userSchema)
+module.exports = userModel
